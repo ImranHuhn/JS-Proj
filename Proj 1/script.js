35ed48c5fbdc8958045ffd9ad7c661c8007cb020
ImranHuhn <huhnimran@gmail.com>
2019-08-21 18:07:58 -0400

    8/21 js exercise

--- Proj 1/script.js
+++ Proj 1/script.js
@@ -1,10 +1,21 @@
-var markMass = Number(prompt("Enter Mark's Mass in kg"));
-var markHeight = Number(prompt("Enter Mark's Height meters"));
-var markBMI = markMass / ( markHeight * markHeight );
+// declare a variable named student with an object inside that has properties name, age, occupation (use your information) then print the variable
 
-var johnMass = Number(prompt("Enter John's Mass in kg"));
-var johnHeight = Number(prompt("Enter John's Height meters"));
-var johnBMI = johnMass / ( johnHeight * johnHeight );
+// add a propety with your birthdate as a String and must be like this object["key"] then print the variable student
 
-alert("Is Mark's BMI larger than John's BMI?");
-alert( markBMI > johnBMI );
+// add an array with all your education but usding .key and inside the array you must have the other objects with the information of each of the schools you went to lik the name, years in the school, and graduationDate and then print the variablw student
+
+// then print one of the schools name
+
+var student = {
+    name: "Imran Huhn",
+    age: 29,
+    occupation: "Student",
+};
+console.log(student);
+student["birthdate"] = "April";
+console.log(student);
+student.education = ["p.s", "i.s", "h.s", "college"];
+console.log(student);
+console.log(student.education[3]);
+
+console.log('==============================');

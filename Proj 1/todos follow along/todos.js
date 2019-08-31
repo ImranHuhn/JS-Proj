50cf27389acf15b463f64cba8d985e1471bb896c
ImranHuhn <huhnimran@gmail.com>
2019-08-30 20:56:40 -0400

    working on mini project

---
+++ Proj 1/todos follow along/todos.js
@@ -0,0 +1,14 @@
+// alert("connected!!"); //test
+var Lis = document.querySelectorAll("li");
+
+for(var i = 0 ; i < Lis.length ; i++) {
+    
+};
+
+
+firstLi.addEventListener("mouseover" , function() {
+    firstLi.style.color="red";
+});
+firstLi.addEventListener("mouseout" , function() {
+    firstLi.style.color="black";
+});

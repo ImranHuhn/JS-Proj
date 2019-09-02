b47ef440207ec589ef04e2af323ce3852a18a4f2
ImranHuhn <huhnimran@gmail.com>
2019-09-02 13:48:01 -0400

    set scores to zero

--- Proj 1/pigGame/app.js
+++ Proj 1/pigGame/app.js
@@ -17,15 +17,43 @@ roundScore = 0;
 activePlayer = 1;
 
 //dices
-dice = Math.floor(Math.random() * 6) + 1;
+// dice = Math.floor(Math.random() * 6) + 1; //use as an event instead
 // console.log(dice);
 
 //set
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
-document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).textContent = dice;
 
 //get
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('#score-0').textContent = '0';
+document.querySelector('#score-1').textContent = '0';
+document.querySelector('#current-0').textContent = '0';
+document.querySelector('#current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click' , function() {
+    // 1. random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    //2.display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    //3.update the round score if the rolled number is NOT 1
+
+});
+
+
+
+
+
+
+
+
+
+

eb6f332b684038e0832a2ff89821f26042b5ec0e
ImranHuhn <huhnimran@gmail.com>
2019-09-02 04:11:39 -0400

    completed mini proj todos app

--- Proj 1/todos follow along/todos.js
+++ Proj 1/todos follow along/todos.js
@@ -2,13 +2,23 @@
 var Lis = document.querySelectorAll("li");
 
 for(var i = 0 ; i < Lis.length ; i++) {
-    
+    Lis[i].addEventListener("mouseover", function() {
+        this.classList.add("selected"); //prefered convention to toggle class instead of styles.
+    });
+
+    Lis[i].addEventListener("mouseout", function() {
+        this.classList.remove("selected"); //prefered convention to toggle class instead of styles.
+    });
+
+    Lis[i].addEventListener("click", function() {
+        this.classList.toggle("done"); //prefered convention to toggle class instead of styles.
+    });
 };
 
 
-firstLi.addEventListener("mouseover" , function() {
-    firstLi.style.color="red";
-});
-firstLi.addEventListener("mouseout" , function() {
-    firstLi.style.color="black";
-});
+// firstLi.addEventListener("mouseover" , function() {
+//     firstLi.style.color="red";
+// });
+// firstLi.addEventListener("mouseout" , function() {
+//     firstLi.style.color="black";
+// });

fb8cf6dbc40187e80fedac287137a88c3e34e94c
ImranHuhn <huhnimran@gmail.com>
2019-09-02 12:49:10 -0400

    worked on the pigGame miniproj

--- Proj 1/pigGame/app.js
+++ Proj 1/pigGame/app.js
@@ -14,7 +14,18 @@ var scores, roundScore, activePlayer, dice;
 //scores
 scores = [0,0];
 roundScore = 0;
-activePlayer = 0;
+activePlayer = 1;
 
 //dices
-dice = 6;
+dice = Math.floor(Math.random() * 6) + 1;
+// console.log(dice);
+
+//set
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
+document.querySelector('#current-' + activePlayer).textContent = dice;
+
+//get
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+document.querySelector('.dice').style.display = 'none';

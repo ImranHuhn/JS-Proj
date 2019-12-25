39ed7e12153a9c02bd22cf14715e48bf86020866
ImranHuhn <huhnimran@gmail.com>
2019-12-25 17:00:47 -0500

    new exercise and color game proj

--- Projects/exercises/exercises.js
+++ Projects/exercises/exercises.js
@@ -419,7 +419,22 @@
 // ======================================
 // ======================================
 
+// var isPurple = false;
 
+// document.querySelector("button").addEventListener("click", function(){
+    
+//     if(isPurple){
+//         document.body.style.background = "white";
+//     } else {
+//         document.body.style.background = "purple";
+//     }
+//     isPurple = !isPurple;
+// });
 
+document.querySelector("button").addEventListener("click", function(){
+        document.body.classList.toggle("purple");
+});
+
+ 
 
 
